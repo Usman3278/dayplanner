8fc54ea97374c9fd4c51b6a9a75e4774511c38b6
Usman3278 <usman3278@gmail.com>
2020-12-15 21:27:48 +0500

    Modifed HTML and JS

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # dayplanner
 
-*Google 
-*w3schools.com
-*github.com
-*stackoverflow.com
-
 This project provides users with a daily planner where they can add their own events to an hourly log for the business hours of 9am-5pm. The planner results in effective time management and greater productivity as users' can easily plan their day ahead.
 
-User Guide
-Enter your notes into the text area for the desired time block and click the save button to store it!
+# User Guide
 
-Grey boxes indicate past hours, red boxes equals the present hour, and green boxes are future hours.
+Enter your notes into the text area for the desired time block and click the save button to save it.
 
-Happy planning!
+# Languages used to generate code quiz are below.
+
+HTML
+CSS
+Javascript
+
+
+The code quiz can be accessed at the following link: https://usman3278.github.io/codequiz/
+
+Screenshots are uploaded into Assests Folder.
 
--- script.js
+++ script.js
@@ -1,209 +1,171 @@
-$(document).ready(function() {
-    // populates current date when dom is loaded
-    var dateParagraph = $("#currentDay");
-    var todayDate = moment().format('dddd, MMMM Do YYYY');
-    dateParagraph.text(todayDate);
-    });
-    
-    //button to clear local storage and refresh the page
-    $("#clear").click(function() {
-        localStorage.clear();
-        location.reload()
-    });
-    
-    
-    //to save the user's input of text into each time slot....
-    
-    var input_textarea1 = document.querySelector('#note1');
-    var save_button1 = document.querySelector('#saveNote1');
-    //retrieves previously save text to display
-    input_textarea1.value = localStorage.getItem('content1');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote1.addEventListener('click', updateOutput1);
-    //sets the input of text area into local storage
-    function updateOutput1() {
-        localStorage.setItem('content1', input_textarea1.value);
-    };
-    
-    var input_textarea2 = document.querySelector('#note2');
-    var save_button2 = document.querySelector('#saveNote2');
-    //retrieves previously save text to display
-    input_textarea2.value = localStorage.getItem('content2');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote2.addEventListener('click', updateOutput2);
-    //sets the input of text area into local storage
-    function updateOutput2() {
-        localStorage.setItem('content2', input_textarea2.value);
-    };
-    
-    var input_textarea3 = document.querySelector('#note3');
-    var save_button3 = document.querySelector('#saveNote3');
-    //retrieves previously save text to display
-    input_textarea3.value = localStorage.getItem('content3');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote3.addEventListener('click', updateOutput3);
-    //sets the input of text area into local storage
-    function updateOutput3() {
-        localStorage.setItem('content3', input_textarea3.value);
-    };
-    
-    var input_textarea4 = document.querySelector('#note4');
-    var save_button4 = document.querySelector('#saveNote4');
-    //retrieves previously save text to display
-    input_textarea4.value = localStorage.getItem('content4');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote4.addEventListener('click', updateOutput4);
-    //sets the input of text area into local storage
-    function updateOutput4() {
-        localStorage.setItem('content4', input_textarea4.value);
-    };
-    
-    var input_textarea5 = document.querySelector('#note5');
-    var save_button5 = document.querySelector('#saveNote5');
-    //retrieves previously save text to display
-    input_textarea5.value = localStorage.getItem('content5');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote5.addEventListener('click', updateOutput5);
-    //sets the input of text area into local storage
-    function updateOutput5() {
-        localStorage.setItem('content5', input_textarea5.value);
-    };
-    
-    var input_textarea6 = document.querySelector('#note6');
-    var save_button6 = document.querySelector('#saveNote6');
-    //retrieves previously save text to display
-    input_textarea6.value = localStorage.getItem('content6');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote6.addEventListener('click', updateOutput6);
-    //sets the input of text area into local storage
-    function updateOutput6() {
-        localStorage.setItem('content6', input_textarea6.value);
-    };
-    
-    var input_textarea7 = document.querySelector('#note7');
-    var save_button7 = document.querySelector('#saveNote7');
-    //retrieves previously save text to display
-    input_textarea7.value = localStorage.getItem('content7');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote7.addEventListener('click', updateOutput7);
-    //sets the input of text area into local storage
-    function updateOutput7() {
-        localStorage.setItem('content7', input_textarea7.value);
-    };
-    
-    var input_textarea8 = document.querySelector('#note8');
-    var save_button8 = document.querySelector('#saveNote8');
-    //retrieves previously save text to display
-    input_textarea8.value = localStorage.getItem('content8');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote8.addEventListener('click', updateOutput8);
-    //sets the input of text area into local storage
-    function updateOutput8() {
-        localStorage.setItem('content8', input_textarea8.value);
-    };
-    
-    var input_textarea9 = document.querySelector('#note9');
-    var save_button9 = document.querySelector('#saveNote9');
-    //retrieves previously save text to display
-    input_textarea9.value = localStorage.getItem('content9');
-    //when the Save button is clicked, store the input of the textarea
-    saveNote9.addEventListener('click', updateOutput9);
-    //sets the input of text area into local storage
-    function updateOutput9() {
-        localStorage.setItem('content9', input_textarea9.value);
-    };
-    
-    
-    //hard code for color coding of time
-    
-    var now= new Date().getHours();
-    
+$(document).ready(function () {
+    // display present day
+    var presentDay = $("#currentDay");
+    var todayDay = moment().format("dddd, MMMM Do YYYY");
+    presentDay.text(todayDay);
+  
+    // color code per time
+    var now = moment().hour();
+    // 9am
     if (now > 9) {
-        $("#note1").addClass("past");
-        }	
-        else if (now >= 9 && now < 10) {
-            $("#note1").addClass("present");
-        }
-        else if (now < 9) {
-            $("#note1").addClass("future");
-        }
-    
+      $("#note1").addClass("past");
+      if (now >= 9 && now < 10) {
+        $("#note1").addClass("present");
+      } else {
+        $("#note1").addClass("future");
+      }
+    }
+    //10 am
     if (now > 10) {
-        $("#note2").addClass("past");
-        }
-        else if (now >= 10 && now < 11) {
-            $("#note2").addClass("present");
-        }
-        else if (now < 10) {
-            $("#note2").addClass("future");
-        }
-    
+      $("#note2").addClass("past");
+      if (now >= 10 && now < 11) {
+        $("#note2").addClass("present");
+      } else {
+        $("#note2").addClass("future");
+      }
+    }
+    // 11 am
     if (now > 11) {
-        $("#note3").addClass("past");
-        }
-        else if (now >= 11 && now < 12) {
-            $("#note3").addClass("present");
-        }
-        else if (now < 11) {
-            $("#note3").addClass("future");
-        }
-    
+      $("#note3").addClass("past");
+      if (now >= 11 && now < 12) {
+        $("#note3").addClass("present");
+      } else {
+        $("#note3").addClass("future");
+      }
+    }
+    // 12 pm
     if (now > 12) {
-        $("#note4").addClass("past");
-        }
-        else if (now >= 12 && now < 13) {
-            $("#note4").addClass("present");
-        }
-        else if (now < 12) {
-            $("#note4").addClass("future");
-        }
-    
+      $("#note4").addClass("past");
+      if (now >= 12 && now < 13) {
+        $("#note4").addClass("present");
+      } else {
+        $("#note4").addClass("future");
+      }
+    }
+    // 1 pm
     if (now > 13) {
-        $("#note5").addClass("past");
-        }
-        else if (now >= 13 && now < 14) {
-            $("#note5").addClass("present");
-        }
-        else if (now < 13) {
-            $("#note5").addClass("future");
-        }
-    
+      $("#note5").addClass("past");
+      if (now >= 13 && now < 14) {
+        $("#note5").addClass("present");
+      } else {
+        $("#note5").addClass("future");
+      }
+    }
+    // 2pm
     if (now > 14) {
-        $("#note6").addClass("past");
-        }
-        else if (now >= 14 && now < 15) {
-            $("#note6").addClass("present");
-        }
-        else if (now < 14) {
-            $("#note6").addClass("future");
-        }
-    
+      $("#note6").addClass("past");
+      if (now >= 14 && now < 15) {
+        $("#note6").addClass("present");
+      } else {
+        $("#note6").addClass("future");
+      }
+    }
+    //3 pm
     if (now > 15) {
-        $("#note7").addClass("past");
-        }
-        else if (now >= 15 && now < 16) {
-            $("#note7").addClass("present");
-        }
-        else if (now < 15) {
-            $("#note7").addClass("future");
-        }
-    
+      $("#note7").addClass("past");
+      if (now >= 15 && now < 16) {
+        $("#note7").addClass("present");
+      } else {
+        $("#note7").addClass("future");
+      }
+    }
+    // 4 pm
     if (now > 16) {
-        $("#note8").addClass("past");
-        }
-        else if (now >= 16 && now < 17) {
-            $("#note8").addClass("present");
-        }
-        else if (now < 16) {
-            $("#note8").addClass("future");
-        }
-    
+      $("#note8").addClass("past");
+      if (now >= 16 && now < 17) {
+        $("#note8").addClass("present");
+      } else {
+        $("#note8").addClass("future");
+      }
+    }
+    //5 pm
     if (now > 17) {
-        $("#note9").addClass("past");
-        }
-        else if (now >= 17 && now < 18) {
-            $("#note9").addClass("present");
-        }
-        else if (now < 17) {
-            $("#note9").addClass("future");
-        }
+      $("#note9").addClass("past");
+      if (now >= 17 && now < 18) {
+        $("#note9").addClass("present");
+      } else {
+        $("#note9").addClass("future");
+      }
+    }
+  
+    // reload previous tasks
+    $("#note1").val(localStorage.getItem("task1"));
+    $("#note2").val(localStorage.getItem("task2"));
+    $("#note3").val(localStorage.getItem("task3"));
+    $("#note4").val(localStorage.getItem("task4"));
+    $("#note5").val(localStorage.getItem("task5"));
+    $("#note6").val(localStorage.getItem("task6"));
+    $("#note7").val(localStorage.getItem("task7"));
+    $("#note8").val(localStorage.getItem("task8"));
+    $("#note9").val(localStorage.getItem("task9"));
+  });
+  
+  //clear local storage and reload
+  $("#clear").click(function () {
+    localStorage.clear();
+    location.reload();
+  });
+  
+  // saving tasks to locaal storage
+  $("#saveNote1").click(updateTask1);
+  function updateTask1() {
+    let task = $("#note1").val();
+    localStorage.setItem("task1", task);
+    console.log(task);
+  }
+  
+  $("#saveNote2").click(updateTask2);
+  function updateTask2() {
+    let task = $("#note2").val();
+    localStorage.setItem("task2", task);
+    console.log(task);
+  }
+  
+  $("#saveNote3").click(updateTask3);
+  function updateTask3() {
+    let task = $("#note3").val();
+    localStorage.setItem("task3", task);
+    console.log(task);
+  }
+  
+  $("#saveNote4").click(updateTask4);
+  function updateTask4() {
+    let task = $("#note4").val();
+    localStorage.setItem("task4", task);
+    console.log(task);
+  }
+  
+  $("#saveNote5").click(updateTask5);
+  function updateTask5() {
+    let task = $("#note5").val();
+    localStorage.setItem("task5", task);
+    console.log(task);
+  }
+  
+  $("#saveNote6").click(updateTask6);
+  function updateTask6() {
+    let task = $("#note6").val();
+    localStorage.setItem("task6", task);
+    console.log(task);
+  }
+  
+  $("#saveNote7").click(updateTask7);
+  function updateTask7() {
+    let task = $("#note7").val();
+    localStorage.setItem("task7", task);
+    console.log(task);
+  }
+  
+  $("#saveNote8").click(updateTask8);
+  function updateTask8() {
+    let task = $("#note8").val();
+    localStorage.setItem("task8", task);
+    console.log(task);
+  }
+  
+  $("#saveNote9").click(updateTask9);
+  function updateTask9() {
+    let task = $("#note9").val();
+    localStorage.setItem("task9", task);
+    console.log(task);
+  }
